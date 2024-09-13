365a365bf55bc5587c973bf918308dbec3f19723
kirill55580666 <kirill55580666@gmail.com>
2024-09-13 11:48:56 +0300

    change query params

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,21 +2,27 @@ import React, {useEffect, useRef, useState} from 'react';
 import '../styles/canvas.scss'
 import canvasState from "../store/canvasState";
 import {observer} from "mobx-react-lite";
-import {useParams} from "react-router-dom";
 import axios from 'axios'
 import MyModal from "./UI/MyModal";
 import firstRender from "../API/firstRender";
 import canvasWebSocket from "../API/canvasWebSocket";
+import { useLocation } from "react-router-dom";
+
+function useQuery() {
+    const { search } = useLocation();
+
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+}
 
 const Canvas = observer(() => {
     const canvasRef = useRef()
     const usernameRef = useRef()
     const [modal, setModal] = useState(true)
-    const params = useParams();
+    const params = useQuery();
 
     useEffect(() => {
         canvasState.setCanvas(canvasRef.current)
-        firstRender(canvasRef, params.id)
+        firstRender(canvasRef, params.get('id'))
         //canvasState.setUsername('New User')
         // axios.get(`http://localhost:5000/history?id=${params.id}`)
         //     .then(response => {
@@ -27,7 +33,7 @@ const Canvas = observer(() => {
 
     useEffect(() => {
         if(canvasState.username) {
-            canvasWebSocket(canvasRef, params.id)
+            canvasWebSocket(canvasRef, params.get('id'))
         }
 
     }, [canvasState.username]) // окрыть сокет, отправлять сообщения серверу о рисовани, принимать сообщения о рисовании
@@ -38,7 +44,7 @@ const Canvas = observer(() => {
     }
 
     const mouseUpHandler = () => {
-        axios.post(`https://paintonline-kirill55580666.amvera.io/image?id=${params.id}`, {img: canvasRef.current.toDataURL()})
+        axios.post(`https://paintonline-kirill55580666.amvera.io/image?id=${params.get('id')}`, {img: canvasRef.current.toDataURL()})
             .then()
     } // каждый действие сохраняем на сервере, для первого рендера новых пользователей
 
